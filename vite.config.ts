1dd749d2cd1b6836d03c4abfb03f21714aa02bb2
dhashdev <dhruv@dhash.dev>
2024-02-02 07:42:10 +0100

    testing if workflows are working

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    exclude: [
-      "**/node_modules/**, **/dist/**, **/cypress/**, **/.{idea,git,cache,output,temp}/**, **/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*",
-    ],
     setupFiles: "./setupTests.ts",
   },
   build: {
